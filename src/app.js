1a75aee67165cadd6899361e883d75a127063786
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2024-11-07 23:43:31 +0100

    refactoring/ fix cors issues

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+
 const subscriberRoutes = require('./routes/subscriberRoutes');
 
 const app = express();
+
+app.use(cors({
+    origin: process.env.ALLOWED_CORS_ORIGIN,
+    methods: ['GET', 'POST'],
+}));
 app.use(express.json());
+
 app.use('/api/subscribers', subscriberRoutes);
 
 module.exports = app;
